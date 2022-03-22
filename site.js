313c3434d7332718904147620e44746721c0de02
Venus <122VHugoWilliams@frhsd.com>
2022-03-22 12:32:13 -0400

    step 3

--- site.js
+++ site.js
@@ -32,8 +32,11 @@ const vue_app = Vue.createApp({
       data() {
         return {
             // This holds your movies.json data.
-            movies: [],
+            movies: [ ],
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+            title: "IMDB + Venus's Top 8 MOVIES",
+            owner: "Venus",
+            github:'https://github.com/vwilliams29/Hugo-P3.git'
       }
     },
       methods: {
