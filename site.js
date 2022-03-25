cc0c6e7ab3c89dba97ed8fcc5d06d1ab23f58b14
Venus <122VHugoWilliams@frhsd.com>
2022-03-25 12:17:51 -0400

    getMonthText

--- site.js
+++ site.js
@@ -41,6 +41,51 @@ const vue_app = Vue.createApp({
     },
       methods: {
             /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+            getMonthText(dateArray){
+              let dateStr = '';
+              let month = dateArray[1];
+              let day = dateArray[2];
+              let year = dateArray[0];
+              // Switch Statement
+              switch (month){
+                case 1:
+                month ='January';
+                break;
+                case 2:
+                month = 'February';
+                break;
+                case 3:
+                month = 'March';
+                break;
+                case 4:
+                month = 'April';
+                break;
+                case 5:
+                month = 'May';
+                break;
+                case 6:
+                month = 'June';
+                break;
+                case 7:
+                month = 'July';
+                break;
+                case 8:
+                month = 'August';
+                break;
+                case 9:
+                month = 'Septemember';
+                break;
+                case 10:
+                month = 'October';
+                break;
+                case 11:
+                month = 'Novemeber';
+                break;
+                case 12:
+                month = 'Decemeber';
+              }
+              return month + ' ' + day + year;
+            }
       }
 })
 
